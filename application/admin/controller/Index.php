b43b75cb8323112e84bfa567a9d59a6a7999793f
teacher-wt <1442247931@qq.com>
2020-03-24 21:07:14 +0800

    项目基本结构

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+
+class Index extends Controller
+{
+    //加载登录页面
+    public function login(){
+        //判断是否为post方式提交
+        if (request()->isPost()){
+            //获取登录信息
+            $data = input('post');
+            //登录信息格式校验
+            $validate = view('');
+
+        }
+        return view('login');
+    }
+
+//    此方法为加载主页面
+    public function index(){
+        return view('index');
+
+    }
+}
